93bb866de6f48d8d7bdb38c2106df6a5d99fa344
Sarhad Bahrami <Sarhad94127@hotmail.com>
2026-02-07 12:38:27 +0100

    Initial commit

---
+++ src/main/java/org/tutorial/venusbackend/VenusBackendApplication.java
@@ -0,0 +1,13 @@
+package org.tutorial.venusbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenusBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VenusBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/tutorial/venusbackend/VenusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tutorial.venusbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenusBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
